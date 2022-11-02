4b91d4cceae1548e8550cb482388809ce585279c
aanc <ana.sanches@gft.com>
2022-11-02 16:08:02 -0300

    Dependicas Auluno/Aula

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -7,7 +7,8 @@ namespace APIEscolaArabe.Models
         public string CPF { get; set; }
         public string Telefone { get; set; }
         public string Matricula { get; set; }
-        public int IdEndereco { get; set; }
-        public int IdCurso { get; set; }
+        public Endereco endereco { get; set; }
+        public Modalidade modalidade { get; set; }
+        public Aula aula { get; set; }
     }
 }
--- Models/Aula.cs
+++ Models/Aula.cs
@@ -5,11 +5,8 @@ namespace APIEscolaArabe.Models
 {
     public class Aula
     {
-
-        //IdModalidade
-        // Aluno? aluno;
-        // aluno.IdAluno;
-
+        public Aluno aluno { get; set; }
+        public Modalidade modalidade { get; set; }
         public int Id { get; set; }
         public DateTime HorarioAula { get; set; }
     }
